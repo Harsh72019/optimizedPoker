6b1d7b1c69c40ec4f560bc7f52b380931e08905f
Harsh Bali <harshbali374@gmail.com>
2026-03-02 16:02:35 +0530

    Changes

--- src/queues/game-queue.js
+++ src/queues/game-queue.js
@@ -7,6 +7,7 @@ const gameQueue = new Queue('game-events', {
     connection: {
         host: config.REDIS_HOST,
         port: config.REDIS_PORT,
+        password: config.REDIS_PASSWORD,
     },
 });
 
